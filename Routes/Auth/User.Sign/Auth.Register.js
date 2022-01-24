78856b326eb514681f5d8669827871b30ef148d3
pritambiswas00 <pritambiswas060@gmail.com>
2022-01-24 22:05:09 +0530

    init commit

--- Routes/Auth/User.Sign/Auth.Register.js
+++ Routes/Auth/User.Sign/Auth.Register.js
@@ -6,6 +6,7 @@ const AuthGuard = require('../../../Middleware/middleware.Users')
 Connection(process.env.USER_DB);
 
 
+
 /** Register Users */
 
 Router.post('/api/register', AuthUsersController.registerUser);
